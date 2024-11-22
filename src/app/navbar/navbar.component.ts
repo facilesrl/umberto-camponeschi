d015d6850397927b05a62b8af383ad4709770b42
carlotta <carlotta@facilesrl.com>
2024-11-22 11:05:18 +0100

    add social-row add rimpicciolimento navbar scrolled

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HostListener } from '@angular/core';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -9,4 +11,17 @@ import { RouterLink } from '@angular/router';
 })
 export class NavbarComponent {
 
+    @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || 0;
+
+    const nav = document.getElementById('navbar');
+    if (nav) {
+      if (scrollPosition > 50) {
+        nav.classList.add('navbar-scrolled');
+      } else {
+        nav.classList.remove('navbar-scrolled');
+      }
+    }
+  }
 }
