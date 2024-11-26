54f8ca7319b81a3bcac9876c2c3301e316ce7fd1
carlotta <carlotta@facilesrl.com>
2024-11-26 19:40:02 +0100

    scroll nell'header su scroll della homepage

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,15 +2,20 @@ import { Component } from '@angular/core';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { CommonModule } from '@angular/common';
+import { HostListener } from '@angular/core';
+import { ScrollManagerComponent } from '../scroll-manager/scroll-manager.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CarouselModule, CommonModule],
+  imports: [CarouselModule, CommonModule,ScrollManagerComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+    translateY: number = 0;
+
     items = [
         { src: 'assets/images/AntonioECleopatra.jpg', alt: 'Descrizione immagine 1', description: 'Antonio e Cleopatra' },
         { src: 'assets/images/Come-gli-uccelli_Ph_Giuseppe-Distefano51_da-sx-Palumeri-Forni.jpg', alt: 'Descrizione immagine 2', description: 'Come gli uccelli' },
@@ -24,6 +29,12 @@ export class HeaderComponent {
         nav: true,
         dots: true
     };
+
+    onScrollReceived(scrollStepChanged: number) {
+        console.log('Received from child:', scrollStepChanged);
+    }
+
+
 
 }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,14 +3,16 @@ import { SplashScreenComponent } from '../splash-screen/splash-screen.component'
 import { ScrollManagerComponent } from '../scroll-manager/scroll-manager.component';
 import { InEvidenzaBoxComponent } from '../in-evidenza-box/in-evidenza-box.component';
 import { HeaderComponent } from '../header/header.component';
-
+import { HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
     selector: 'app-home',
     standalone: true,
     imports: [  SplashScreenComponent, 
                 ScrollManagerComponent, 
                 InEvidenzaBoxComponent,
-                HeaderComponent],
+                HeaderComponent,
+                CommonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
@@ -23,4 +25,24 @@ export class HomeComponent {
         this.currentStep = step;  // Aggiorna currentStep con il nuovo valore
     }
 
+    translateY:number = 0;
+
+    @HostListener('wheel', ['$event'])
+    onWheelScroll(event: WheelEvent) {
+        const scrollDelta = event.deltaY;  // Movimento verticale della rotella
+        this.translateY += scrollDelta/4;
+       const minTranslate :number = 0;
+       const maxTranslate :number = 150;
+
+       if(this.translateY>maxTranslate){
+        this.translateY=150;
+       }
+       if(this.translateY<minTranslate){
+        this.translateY=0;
+       }
+    }
+
+    lazyScroll(): string {
+       return `translateY(${this.translateY}px)`
+    }
 }
